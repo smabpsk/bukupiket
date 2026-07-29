f8005b27106544d45d55ccc80a6bad35da7b4967
smabpsk <bukupiketsmabpsk@gmail.com>
2026-07-29 12:40:50 +0700

    config.php

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 session_start();
 
 // GANTI DENGAN URL WEB APP GOOGLE APPS SCRIPT ANDA
-define('GAS_API_URL', 'https://script.google.com/macros/s/AKfycbwnwiTBWCE4OvV9grPkqHMZ4BmZPm3GA934z4MM4einjHwvTXfOAFuFVjqVm1QXA9M/exec');
+define('GAS_API_URL', 'https://script.google.com/macros/s/AKfycbyAyUqtaPjydMIatFVEBA-snzP-N7bezXAKQzaGHbD86JMB9YLGJMU2JPKR6_FxUnyi/exec');
 define('APP_NAME', 'Buku Piket Digital');
 ?>
