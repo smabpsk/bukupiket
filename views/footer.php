70df1b8aac2dc9dd14eac127f4f2d16ea6d1dec5
smabpsk <bukupiketsmabpsk@gmail.com>
2026-07-29 12:28:36 +0700

    footer.php

---
+++ views/footer.php
@@ -0,0 +1,7 @@
+<footer class="text-center py-3 mt-4" style="background-color: #ffffff; border-top: 1px solid #dee2e6;">
+    <small><span id="footer-school-name">
+            <?= htmlspecialchars($sekolahInfo['nama_sekolah']) ?>
+        </span> | Design by SMA BPS&K I Jakarta
+        <?= date('Y') ?>
+    </small>
+</footer>
